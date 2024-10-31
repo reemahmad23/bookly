d293d98400c720bca78a8e44d38fdc1e12623801
reemahmad <reemmahmad40@gmail.com>
2024-10-31 11:12:41 +0200

    splash view body done

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_appl/core/utlis/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(AssetsData.logo),
+      ],
+    );
+  }
+}
