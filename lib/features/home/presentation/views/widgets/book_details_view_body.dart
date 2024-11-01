79e98b316d4b34433bc7b896a20c92cd3b34a32f
reemahmad <reemmahmad40@gmail.com>
2024-11-01 18:12:22 +0200

    add rating

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,6 @@
+import 'package:bookly_appl/core/utlis/styles.dart';
+import 'package:bookly_appl/features/home/presentation/views/widgets/book_action.dart';
+import 'package:bookly_appl/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_appl/features/home/presentation/views/widgets/ccustom_list_view_item.dart';
 import 'package:bookly_appl/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +14,7 @@ class BookDetailsViewBody extends StatelessWidget {
     return  Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           CustomBookDetailsAppBar(),
 
@@ -18,6 +22,36 @@ class BookDetailsViewBody extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: width * .34),
             child: CustomListViewItem(),
           ),
+          SizedBox(
+          height: 10,
+        ),
+        Text(
+          'Amarita & Zicola',
+          style: Styles.textStyle30,
+        ),
+        SizedBox(
+          height: 3,
+        ),
+        Opacity(
+          opacity: .7,
+          child: Text(
+            'Amr Abdelhamied',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        BookAction(),
         ],
       ),
     );
--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -4,18 +4,27 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating({super.key,  this.mainAxisAlignment = MainAxisAlignment.start });
 
+final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         Icon(
           FontAwesomeIcons.solidStar,
-          color: Color(0xffFFDDF),
+          color: Colors.yellow,
+          size: 18,
+        ),
+        SizedBox(
+          width: 8,
         ),
         Text('4.9',
         style: Styles.textStyle16,
+        ),
+        SizedBox(
+          width: 8,
         ),
         Text('(245)',
         style: Styles.textStyle14.copyWith(
